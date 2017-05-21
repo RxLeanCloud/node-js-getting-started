be84a910151c081186f5893da3158ba2d6eb066f
WuJun <wujun19890209@gmail.com>
2017-05-21 19:28:31 +0800

    feat: replace leanengine node sdk.

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 'use strict';
 var AV = require('leanengine');
+var RxAVClient = require('rx-lean-js-core').RxAVClient;
+var RxAVApp = require('rx-lean-js-core').RxAVApp;
 
-AV.init({
+var app = new RxAVApp({
   appId: process.env.LEANCLOUD_APP_ID,
-  appKey: process.env.LEANCLOUD_APP_KEY,
-  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
+  appKey: process.env.LEANCLOUD_APP_KEY
 });
 
-// 如果不希望使用 masterKey 权限，可以将下面一行删除
-AV.Cloud.useMasterKey();
+RxAVClient.init(
+  {
+    log: true
+  }).add(app);
 
 var app = require('./app');
 
@@ -20,10 +23,10 @@ app.listen(PORT, function (err) {
   console.log('Node app is running on port:', PORT);
 
   // 注册全局未捕获异常处理器
-  process.on('uncaughtException', function(err) {
+  process.on('uncaughtException', function (err) {
     console.error("Caught exception:", err.stack);
   });
-  process.on('unhandledRejection', function(reason, p) {
+  process.on('unhandledRejection', function (reason, p) {
     console.error("Unhandled Rejection at: Promise ", p, " reason: ", reason.stack);
   });
 });
